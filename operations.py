51474642ff181b6da8fc7c1f0c7a70ab60916518
ternarycode <102872137+ternarycode@users.noreply.github.com>
2022-05-04 12:21:23 +0800

    Update operations.py

--- operations.py
+++ operations.py
@@ -1,12 +1,15 @@
+# input some number...
 answer = input("Please enter a number: ")
 print("")
 
+# then it'll do operations by/to 7
 adds = int(answer) + 7
 diff = int(answer) - 7
 prod = int(answer) * 7
 quot = float(answer) / 7.0
 mod = int(answer) % 7
 
+#then, print!
 print("The sum of",answer,"and 7 is",adds)
 print("The difference of",answer,"and 7 is",diff)
 print("The product of",answer,"and 7 is",prod)
