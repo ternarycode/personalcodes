285e3da5c382259d8bd0232ebbe4a745a34307f7
ternarycode <102872137+ternarycode@users.noreply.github.com>
2022-05-04 12:01:29 +0800

    Update ascii_code.c

--- ascii_code.c
+++ ascii_code.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
 #include <unistd.h>
 
+//this block is for printing it out once
 int main(){
 	char startLetter = 'A';
 	for(startLetter; startLetter; startLetter++){
+			printf("%c ", startLetter); 
+		}
+	
+	return 0;
+}
+
+//and this one will COMPLETELY GAME END YOUR COMPUTER
+int main(){
+	char startLetter = 'A';
+	for(startLetter; startLetter == startLetter; startLetter++){
 			printf("%c ", startLetter); 
 		}
 	
